bbf1fc860a191b85e6ac265d1c67db76a29a6d35
Mykola Pustovoychenko <pustovoychenko.mykola@gmail.com>
2018-01-18 14:21:01 +0200

    autodetermine itemid

--- tunnel-utils.js
+++ tunnel-utils.js
@@ -1,17 +1,45 @@
 class TunnelUtils {
-	static searchForEmbeddedItem() {
+	static searchForItemId() {
 		return executeEmbedSearch();
 	}
 }
 
 const executeEmbedSearch = (fileName) => {
+	let script;
+
+	if (isPlaybuzzDomain()) {
+		script = getNativeItemId;
+	} else {
+		script = getEmbeddedItemId;
+	}
+
 	return new Promise((resolve, reject) => {
-	    chrome.tabs.executeScript(null, {code:`
-	    		document.querySelector('.playbuzz').getAttribute('data-id');
-			`}, 
+	    chrome.tabs.executeScript(null, {code:script}, 
 			(results) => {
 		        resolve(results);
 		    }
 	    );
+	});
+};
+
+const getEmbeddedItemId = `document.querySelector('.playbuzz').getAttribute('data-id');`;
+const getNativeItemId = `document.body.getAttribute('data-gameid');`;
+
+const isPlaybuzzDomain = () => {
+	return getCurrentUrl().then(url => {
+		return url.indexOf('playbuzz.com') !== -1;
+	});
+};
+
+const getCurrentUrl = () => {
+	return new Promise((resolve, reject) => {
+		const queryInfo = {
+			active: true,
+			currentWindow: true
+		};
+
+		chrome.tabs.query(queryInfo, (tabs) => {
+			resolve(tabs[0].url);
+		});
 	});
 };
