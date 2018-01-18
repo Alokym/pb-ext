526b5aba76512f0509253778ab9f6e6417da2523
iddanh <iddanha@playbuzz.com>
2018-01-18 16:40:27 +0200

    feat(): more work

--- popup.js
+++ popup.js
@@ -1,59 +1,53 @@
 const UNSPLASH_ID = '9bd95b5a14ec487142232438d9994dfa42197c41397e3018a29bfcc9e12c0720';
 const UNSPLAHS_URL = 'https://api.unsplash.com/';
 
-const PROD_USER_SERVICE = 'http://user.playbuzz.com/user/current';
-const ALPHA_USER_SERVICE = 'http://stg-user.playbuzz.com/user/current';
-
 function closePopup() {
     window.close();
 }
 
 function getUserDetails() {
-    chrome.cookies.getAll({ name: 'playbuzz' }, cookies => {
-        if (cookies.length <= 0) {
-            return;
-        }
+    return new Promise((resolve, reject) => {
+        chrome.cookies.getAll({ name: 'playbuzz' }, cookies => {
+            if (cookies.length <= 0) {
+                return;
+            }
 
-        let cookieHeader = `${cookies[0].name}=${cookies[0].value}`;
-        console.log(cookieHeader);
-        getEnvironment()
-            .then((env) => {
-                let path = '';
-                if (env === 'production') {
-                    path = PROD_USER_SERVICE;
-                } else {
-                    path = ALPHA_USER_SERVICE;
-                }
-
-                console.log(path);
-
-                return fetch(path, {
-                    method: 'GET',
-                    headers: { 'Cookie': cookieHeader }
-                });
-            })
-            .then(res => {
-                console.log(res);
-            });
+            return resolve(JSON.parse(decodeURIComponent(cookies[0].value)));
+        });
     });
+}
 
+function setLink(url) {
+    document.getElementById('link').innerText = url;
+
+    document.getElementById('link-wrapper').addEventListener('click', () => {
+        chrome.tabs.create({ url: url });
+    });
+}
+
+function setSlackLink() {
+
+}
+
+function changeView() {
+    document.getElementById('progress').style = 'display: none';
+    document.getElementById('complete').style = 'display: block';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // setTimeout(() => {
-    //
-    // }, 2000);
-
-    // getUserDetails();
-
-    // fetch('https://www.google.com')
-    //     .then(res => console.log(res.text()))
+    getUserDetails().then(details => console.log(details));
 
     document.getElementById('close').addEventListener('click', closePopup);
 
-    TunnelUtils.performTunneling().then(() => {
-        document.getElementById('progress').style = 'display: none';
-        document.getElementById('complete').style = 'display: block';
-    });
+    setTimeout(() => {
+        TunnelUtils.performTunneling().then((url) => {
+            url = 'https://www.google.com';
+            setLink(url);
+
+            setSlackLink();
+
+            changeView();
+        });
+    }, 1000);
 });
